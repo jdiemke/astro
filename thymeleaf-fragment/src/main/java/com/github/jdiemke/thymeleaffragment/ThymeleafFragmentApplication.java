b61671b8a56e81ba53d30c301c1c625680a5f1c0
jdiemke <johannes.diemke@eventim.de>
2026-01-06 17:46:56 +0100

    add thymeleaf fragment

---
+++ thymeleaf-fragment/src/main/java/com/github/jdiemke/thymeleaffragment/ThymeleafFragmentApplication.java
@@ -0,0 +1,13 @@
+package com.github.jdiemke.thymeleaffragment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFragmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafFragmentApplication.class, args);
+	}
+
+}
---
+++ thymeleaf-fragment/src/test/java/com/github/jdiemke/thymeleaffragment/ThymeleafFragmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jdiemke.thymeleaffragment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafFragmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
