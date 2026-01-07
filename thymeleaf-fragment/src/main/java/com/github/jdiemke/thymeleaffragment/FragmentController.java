abdcd6d6b98bc0f1eacf14de0ea58cf3acbf00e1
jdiemke <johannes.diemke@eventim.de>
2026-01-07 01:00:08 +0100

    add some pokemons

--- thymeleaf-fragment/src/main/java/com/github/jdiemke/thymeleaffragment/FragmentController.java
+++ thymeleaf-fragment/src/main/java/com/github/jdiemke/thymeleaffragment/FragmentController.java
@@ -4,12 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+
 @Controller
 public class FragmentController {
 
     @GetMapping
     String getFragmentTemplate(Model model) {
         model.addAttribute("name", "Johannes");
+        model.addAttribute("team", Arrays.asList(
+                "Swampert",
+                "Zoroark",
+                "Torterra"
+        ));
 
         return "fragment";
     }
